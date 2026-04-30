6831b5376483b33b52156be3721a01353d266eb2
PsychoShinigami <ayushrajkeshri95@gmail.com>
2026-04-30 15:09:33 +0530

    Learnt flask for the login system

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+import json
+import os
+
+app = Flask(__name__)
+
+def save_user(new_user):
+    if os.path.exists('info.json'):
+        with open('info.json','r') as f:
+            users=json.load(f)
+    else:
+        users=[]
+
+    users.append(new_user)
+
+    with open('info.json', 'w') as f:
+        json.dump(users, f, indent=4)
+
+@app.route('/register', methods=['POST'])
+def register():
+    data=request.get_json()
+    save_user(data)
+    return jsonify({"status": "success", "message": "User registered!"})
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    with open('info.json', 'r') as f:
+        all_users=json.load(f)
+    for i in all_users:
+        if i['username'] == data['username'] and i['password'] == data['password']:
+            return jsonify({"status": "success", "message": "Login Successful!"})
+    return jsonify({"status": "error", "message": "User not found or wrong password!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
